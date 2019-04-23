2e003a8b73df81ec9b56eabeb70078a4b561ff42
Rodolfo <jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.com>
2019-04-23 18:17:02 -0500

    Updates

---
+++ precache-manifest.fa21e3e2eb6e63ea20d3caf2bd4d1b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e26e801e3a888f9aa1256c755c1ca8",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9ec357c7c39b2b8c989a",
+    "url": "/clicky-game/static/css/main.f0a4d752.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "9ec357c7c39b2b8c989a",
+    "url": "/clicky-game/static/js/main.a7ab78f3.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
